63604fdc893b313e96d44fd6861ebf881037a0f4
Agintropotoponim <karaspinf@gmail.com>
2026-05-01 18:40:14 +0400

    feat: add parallel fetching to comments

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,16 +13,7 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
     try {
-        // Artificially delay a response for demo purposes.
-        // Don't do this in production :)
-
-        // console.log('Fetching revenue data...');
-        // await new Promise((resolve) => setTimeout(resolve, 3000));
-
         const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-        // console.log('Data fetch completed after 3 seconds.');
-
         return data;
     } catch (error) {
         console.error('Database Error:', error);
@@ -84,6 +75,26 @@ export async function fetchCardData() {
         throw new Error('Failed to fetch card data.');
     }
 }
+
+// export async function fetchCardData() {
+//     try {
+//         const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+//         const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
+//         const invoiceStatusPromise = sql`SELECT
+//             SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
+//             SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
+//             FROM invoices`;
+
+//         const data = await Promise.all([
+//             invoiceCountPromise,
+//             customerCountPromise,
+//             invoiceStatusPromise,
+//         ]);
+//         // ...
+//     } catch (e) {
+//         // ...
+//     }
+// }
 
 const ITEMS_PER_PAGE = 6;
 export async function fetchFilteredInvoices(
