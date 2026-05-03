46269011fabbcc7516087ce345530f6063e0df6b
Agintropotoponim <karaspinf@gmail.com>
2026-05-03 18:53:32 +0400

    feat: add server action for invoice editing

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -22,6 +22,11 @@ const CreateInvoice = FormSchema.omit({
     date: true,
 });
 
+const UpdateInvoice = FormSchema.omit({
+    id: true,
+    date: true,
+});
+
 export async function createInvoice(formData: FormData) {
     const {
         customerId,
@@ -42,4 +47,30 @@ export async function createInvoice(formData: FormData) {
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices')
+}
+
+export async function updateInvoice(
+    id: string,
+    formData: FormData
+) {
+    const {
+        customerId,
+        amount,
+        status,
+    } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
