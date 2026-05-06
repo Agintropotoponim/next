01a2126cd18b36559c6604002646e75372815d4b
Agintropotoponim <karaspinf@gmail.com>
2026-05-06 14:42:25 +0400

    feat: add customers page

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,16 @@
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+        page?: string;
+    }>;
+}) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+
+    const customers = await fetchFilteredCustomers(query);
+
+    return <CustomersTable customers={customers} />;
+}
